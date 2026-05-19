25ed0117a41b80ba5f279b2e9ea10b7b0ef136ac
amoresvictorbaliad05-eng <amoresvictorbaliad@gmail.com>
2026-05-19 22:33:21 +0800

    minor fixes

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import * as passport from "passport";
-import * as session from "express-session";
+import session from "express-session";
 import setupPassport from "./config/passport";
 
 // Import routes
